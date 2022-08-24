3db15a8b289e364d41464bd21b4cde622955f4e5
mshah245 <93416417+mshah972@users.noreply.github.com>
2022-08-24 13:35:51 -0500

    Made StudentClasses

--- Student Report Management/SRM.cpp
+++ Student Report Management/SRM.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -14,4 +15,27 @@ int main() {
     cout << "HI!" << endl;
 
     return 0;
+}
+
+
+void StudentClasses(string classes, int courseID, string courseLetters, int creditHours, int hallNum, string hallName)
+{
+    string Class = classes;
+    int courseID = courseID;
+    string courseLetters = courseLetters;
+    int creditHours = creditHours;
+    int hallNum = hallNum;
+    string hallName = hallName;
+
+
+    //create a file
+    ofstream outFile;
+    outFile.open("Classes.txt");
+    outFile << Class << endl;
+    outFile << courseLetters << " " << courseID << " : " <<  creditHours << endl;
+    outFile << hallNum << " " << hallName << endl;
+    outFile << endl;
+    outFile.close();
+
+
 }
