665e0a51551ca08e01f6bb657df847e6d5710bf4
mshah245 <93416417+mshah972@users.noreply.github.com>
2022-08-24 13:19:43 -0500

    Start of this Project

---
+++ Student Report Management/SRM.cpp
@@ -0,0 +1,17 @@
+/***********************************************
+ Moksh Shah
+ 08/28/2022
+ Language: C++
+ ***********************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    cout << "HI!" << endl;
+
+    return 0;
+}
